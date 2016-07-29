cb93ae6905ead02f428c40f1fcf979e7cbb20890
Chris Nelson <c.m.nelson.fitt@gmail.com>
2016-07-29 12:01:15 -0600

    Update

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 $("a > svg").css("fill", "#39ADD1");
-$("a").mouseover(function(){$(this).children().css("fill", "black");}).mouseout(function(){$(this).children().css("fill", "#39ADD1");});
+$("a").mouseover(function(){$(this).children().css("fill", "black");
+    }).mouseout(function(){$(this).children().css("fill", "#39ADD1");});
